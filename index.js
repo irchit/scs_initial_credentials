824c3e281707ef71d6102d9872ec52cec4d11b10
Chițea Răzvan <rchitea@gmail.com>
2023-11-06 14:17:47 +0200

    new updates

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+function tempAlert(msg,duration)
+{
+    var el = document.createElement("div");
+    el.setAttribute(
+        "style",
+        "opacity: 0;transition: all 0.3s ease-in-out;font-family: 'Consolas', sans-serif; font-size: 1.6vh; position:fixed;bottom: 5%; right: 2%;background-color:#4C4E52;color: white; width: 20%; border: solid 2px black;border-radius: 10px;text-align: center; padding: 10px;"
+    );
+    el.innerHTML = msg;
+    setTimeout(
+        function(){
+            el.style.opacity = '1';
+        },
+        100
+    );
+    setTimeout(
+        function(){
+            el.parentNode.removeChild(el);
+        },
+        duration
+    );
+    document.body.appendChild(el);
+}
+
 function copyToClickboard( id_text ){
 
     var textbox = document.getElementById(id_text);
 
     navigator.clipboard.writeText(textbox.innerText);
-
-    alert("Password copied to clipboard ✔");
+    tempAlert("Copied to clipboard", 1500);
 }
 
 function connectToMicrosoft(){
